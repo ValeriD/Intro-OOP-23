5168c96a3e716bd16a18fc7ab1a738b4a961bc81
Valeri Dobrev <valeri.dobrev10@gmail.com>
2023-02-28 10:10:35 +0200

    Homework

--- 10g/lesson1/main.cpp
+++ 10g/lesson1/main.cpp
@@ -9,6 +9,12 @@ struct vector_t
     size_t capacity;
 };
 
+/**
+ * @brief Function for creating new object of type vector_t
+ *
+ * @param size
+ * @return struct vector_t*
+ */
 struct vector_t *init()
 {
     struct vector_t *vector = new vector_t;
@@ -18,6 +24,11 @@ struct vector_t *init()
     return vector;
 }
 
+/**
+ * @brief Function for freeing the memory allocated for a vector
+ *
+ * @param v
+ */
 void deleteVector(vector_t *vector)
 {
     // for (size_t i = 0; i < vector->capacity; i++)
@@ -28,6 +39,11 @@ void deleteVector(vector_t *vector)
     delete vector;
 }
 
+/**
+ * @brief Resizes the vector with double the current capacity
+ *
+ * @param vector
+ */
 void resize(vector_t *vector)
 {
     int *newArray = new int[vector->capacity * 2];
@@ -39,6 +55,12 @@ void resize(vector_t *vector)
     vector->data = newArray;
 }
 
+/**
+ * @brief Function for appending new element to the array
+ *
+ * @param vector
+ * @param v
+ */
 void add(vector_t *vector, int value)
 {
     if (vector->size == vector->capacity)
@@ -48,6 +70,34 @@ void add(vector_t *vector, int value)
     vector->data[vector->size] = value;
     vector->size++;
 }
+
+//============================================================================
+// HOMEWORK
+//============================================================================
+/**
+ * @brief Create exact clone of a vector
+ *
+ * @param vector
+ * @return struct vector_t*
+ */
+struct vector_t *clone(struct vector_t *vector);
+
+/**
+ * @brief Removes the last element of a vector
+ *
+ * @param vector
+ * @return struct vector_t*
+ */
+struct vector_t *removeLast(struct vector_t *vector);
+
+/**
+ * @brief Retrieves the element at the provided index
+ *
+ * @param vector
+ * @param index
+ * @return int
+ */
+int at(struct vector_t *vector, size_t index);
 
 int main()
 {
---
+++ 10g/lesson2/hw.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+class Theatre
+{
+    // Add fields:
+    // Number of total tickets to stop and one
+    // Number of sold tickets
+
+public:
+    /**
+     * @brief Construct a new empty Parking object - 0 tickets, 0 taken
+     *
+     */
+    Theatre();
+
+    /**
+     * @brief Construct a new Parking object with total tickets and 0 takne
+     *
+     * @param totalTickets
+     */
+    Theatre(int totalTickets);
+
+    /**
+     * @brief Construct a new Parking object with total tickets and taken tickets
+     *
+     * @param totaltickets
+     * @param soldTickets
+     */
+    Theatre(int totalTickets, int soldTickets);
+
+    /**
+     * @brief Get the total free tickets in the parking lot
+     *
+     */
+    int getTotalAvailableTickets() const;
+};
+
+int main()
+{
+    /* TODO remove this comment to test the functionalities
+    Parking p1;
+    cout << "There should be 0 tickets, and there are: " << p1.getTotalAvailableTickets() << endl;
+
+    Parking p2(10);
+    cout << "There should be 10 tickets, and there are: " << p2.getTotalAvailableTickets() << endl;
+
+    Parking p3(10, 5);
+    cout << "There should be 5 tickets, and there are: " << p2.getTotalAvailableTickets() << endl;
+    */
+}
