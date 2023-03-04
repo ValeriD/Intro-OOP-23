b300efd642b55238668f24592f88301a0a31328a
Valeri Dobrev <valeri.dobrev10@gmail.com>
2023-03-04 16:59:01 +0200

    Uploaded a HW

---
+++ 10g/lesson3/hw.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+class String
+{
+    char *data;
+    size_t length;
+
+public:
+    String(const char *data);
+
+    String(const String &other);
+
+    String &operator=(const String &other);
+
+    ~String();
+
+    const char *getCString() const
+    {
+        return this->data;
+    }
+
+    size_t length() const
+    {
+        return this->length;
+    }
+};
+
+int main()
+{
+    String myString("Test message");
+
+    cout << "Expected: Test message, got: " << myString.getCString() << endl;
+
+    String stringFromCopyConstructor(myString);
+    cout << "Expected: Test message, got: " << stringFromCopyConstructor.getCString() << endl;
+
+    String string("Test message 2");
+
+    string = myString;
+    cout << "Expected: Test message 2, got: " << string.getCString() << endl;
+}
