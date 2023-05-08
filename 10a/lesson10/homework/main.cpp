fe69320255fa56672f6d56684edd5d70429ff724
Valeri Dobrev <valeri.dobrev10@gmail.com>
2023-05-08 18:06:53 +0300

    Solution to the homework

--- 10a/lesson10/homework/main.cpp
+++ 10a/lesson10/homework/main.cpp
@@ -7,28 +7,98 @@ using namespace std;
 
 class Dress
 {
-    // brand -string , color - string, bust - double,  waist - double, hips - double,
+    string brand;
+    string colour;
+    double bust, hips, waist;
+    bool checkFivePercent(double n1, double n2) const
+    {
+        return n2 >= n1 * 0.95 && n2 <= n1 * 1.05;
+    }
+
 public:
-    // TODO empty strings and zero values
-    Dress();
+    Dress()
+    {
+        brand = "";
+        colour = "";
+        bust = 0;
+        hips = 0;
+        waist = 0;
+    }
 
     // Checks if the dress fits and there should be 5% margin
-    bool doesFit(double bust, double hips, double waist);
+    bool doesFit(double bust, double waist, double hips) const
+    {
+        return checkFivePercent(this->bust, bust) &&
+               checkFivePercent(this->hips, hips) &&
+               checkFivePercent(this->waist, waist);
+    }
 
-    friend ostream &operator<<(ostream &out, const Dress &d);
+    friend ostream &operator<<(ostream &out, const Dress &d)
+    {
+        out << d.brand << "\n"
+            << d.colour << "\n"
+            << d.bust << " " << d.hips << " " << d.waist << endl;
+        return out;
+    }
 
-    friend istream operator>>(istream &in, const Dress &d);
+    friend istream &operator>>(istream &in, Dress &d)
+    {
+        getline(in, d.brand);
+        getline(in, d.colour);
+
+        string measurements;
+        getline(in, measurements);
+        stringstream measurementsStream(measurements);
+
+        measurementsStream >> d.bust >> d.hips >> d.waist;
+        return in;
+    }
 };
 
 int main()
 {
     vector<Dress> dresses;
 
-    // TODO load all dresses from a file
+    fstream f;
+    f.open("dresses.txt", ios::in);
+    if (!f.is_open())
+    {
+        cout << "Error: Could not open file dresses.txt!" << endl;
+        return -1;
+    }
+
+    while (!f.eof())
+    {
+        Dress d;
+        f >> d;
+        dresses.push_back(d);
+    }
+    f.close();
 
     cout << "Enter your measurements: "; // - bust, waist, hips
-    // TODO load the user's measurements from the console
+    string measurements;
+    getline(cin, measurements);
+    stringstream measurementsStream(measurements);
+    double bust, waist, hips;
+    measurementsStream >> bust >> hips >> waist;
 
-    // TODO print the dresses that fit the user in the console, and save them in a file
+    f.open("matches.txt", ios::out | ios::trunc);
+    if (!f.is_open())
+    {
+        cout << "Error: Could not open file matches.txt!" << endl;
+        return -1;
+    }
+
+    for (const Dress &d : dresses)
+    {
+        if (d.doesFit(bust, waist, hips))
+        {
+            f << d;
+            cout << d;
+        }
+    }
+
+    f.close();
+
     return 0;
 }
